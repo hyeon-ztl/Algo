84bf84273b684447495765ddb4106db09337f50d
hyeon-ztl <gusto917@naver.com>
2025-07-09 22:54:35 +0900

    [Gold V] Title: 동전 2, Time: 116 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -1,5 +1,4 @@
 
-
 import java.util.*;
 import java.io.*;
 
@@ -33,15 +32,38 @@ public class Main {
          */
 
         int [] dp = new int [value+1];
+        Arrays.fill(dp, INF);
         dp[0] = 0;
 
-        for (int i= 1; i <= value; i++){
-            dp[i] = INF;
-            for(int coin : coins){
-                if(i-coin < 0 || dp[i-coin] == INF) continue;
-                dp[i] = Math.min(dp[i-coin]+1, dp[i]);
+//        for (int i= 1; i <= value; i++){
+//            for(int coin : coins){
+//                if(i-coin < 0 || dp[i-coin] == INF) continue;
+//                dp[i] = Math.min(dp[i-coin]+1, dp[i]);
+//            }
+//        }
+//
+//        for(int j =0; j< coins.length; j++) {
+//            if(j==0){
+//                for (int i=coins[1]; i<= value; i++){
+//                    if(i-coins[j]<0 || dp[i-coins[j]==-1) dp[i] = -1;
+//                    dp[i] = dp[i-coins[j]]+1;
+//                }
+//            }
+//            else{
+//                for (int i=coins[1]; i<= value; i++){
+//                    dp[i] = Math.min(dp[i-coins[j]]+1, dp[i]);
+//                }
+//            }
+//
+//        }
+
+        for (int coin : coins){
+            for(int i=coin; i<=value; i++){
+                if(dp[i-coin] == INF) continue;
+                dp[i] = Math.min(dp[i], dp[i-coin]+1); // 항상 coin이 몇번째건 그 몇번째 줄은 코인 가짓수가 늘어날 수록 최소로 사용할수 있는 동전 수는 줄어듬 ! 반대로 조합은 늘어나고 그래서 가능
             }
         }
+
         System.out.println(dp[value]==INF?-1:dp[value]);
 
     }
