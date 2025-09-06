0fa0e93a5ca1d2e64230c2fd8aee284e2ae27e2b
hyeon-ztl <gusto917@naver.com>
2025-09-06 10:07:26 +0900

    [level 3] Title: 입국심사, Time: 55.66 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,27 +1,36 @@
+import java.io.*;
 import java.util.*;
 
 class Solution {
     public long solution(int n, int[] times) {
+        long answer = 0;
         long left = 1;
-        long right = (long) Arrays.stream(times).max().getAsInt() * n;
-        long answer = right;
-
-        while (left <= right) {
-            long mid = (left + right) / 2;
-
-            long people = 0;
-            for (int time : times) {
-                people += mid / time;
-            }
-
-            if (people >= n) {
-                answer = mid;
-                right = mid - 1; // 더 적은 시간으로도 가능할 수 있음
-            } else {
-                left = mid + 1; // 더 많은 시간이 필요
-            }
+        long right = 0;
+        
+        for(int time : times){
+           right = Math.max(right, (long)time*(long)n);
         }
-
+        long mid = 0;
+        
+        while (left <= right){
+             mid = (left + right)/2;
+            
+            long people = 0; 
+            for(int time : times){
+                people += mid /time;
+            }
+            // System.out.println("mid: "+mid +"\n people: "+people);
+            
+            if(people >= n){ // 처리량이 더많다, 시간이 남음
+                answer = mid;
+                right = mid -1;
+            }
+            else if(people < n){ // 처리량이 더 적다 , 시간 부족
+                left = mid +1;
+            }    
+        }
+        
+        
         return answer;
     }
 }
